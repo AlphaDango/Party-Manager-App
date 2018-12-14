6f9c9f83ed9e8426e1a90537be160294d4d9a27c
Your Name <you@example.com>
2018-12-14 14:06:55 +0100

    Menü eingefügt um zwischen Login und Map auszuwählen

---
+++ app/src/main/java/lxnkn/bearoundwithparty/LoginActivity.java
@@ -0,0 +1,36 @@
+package lxnkn.bearoundwithparty;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.login:
+                startActivity(new Intent(this,LoginActivity.class));
+                return true;
+            case R.id.map:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
