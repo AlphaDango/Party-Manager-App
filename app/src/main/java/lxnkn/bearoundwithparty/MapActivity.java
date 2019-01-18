2f7003813fba59d91dfaff1386a953f9fa258456
Oliver <on18@tu-clausthal.de>
2019-01-18 14:31:40 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/lxnkn/bearoundwithparty/MapActivity.java
+++ app/src/main/java/lxnkn/bearoundwithparty/MapActivity.java
@@ -1,5 +1,6 @@
 package lxnkn.bearoundwithparty;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -7,12 +8,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import static lxnkn.bearoundwithparty.util.constants.Lat;
+import static lxnkn.bearoundwithparty.util.constants.Lng;
+import static lxnkn.bearoundwithparty.util.constants.Locations;
 import static lxnkn.bearoundwithparty.util.constants.MAPVIEW_BUNDLE_KEY;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -69,14 +77,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMapView.onStop();
     }
 
+    private Location getLocation(){
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager
+                .getBestProvider(criteria, false));
+
+        return location;
+    }
+
+
     @Override
     public void onMapReady(GoogleMap map) {
         if(zeit > 17 || zeit < 7)
             map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_night));
         else
             map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_day));
-        map.addMarker(new MarkerOptions().position(new LatLng(51.805387, 10.346454)).title("VdSt"));
+        for(int i = 0; i < Locations.length; i++){
+            map.addMarker(new MarkerOptions().position(new LatLng(Lat[i], Lng[i])).title(Locations[i]));
+        }
         map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(getLocation().getLatitude(), getLocation().getLongitude()), 2));
     }
 
     @Override
